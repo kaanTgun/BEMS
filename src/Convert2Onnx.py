0b72219650d4e770a107c9f625618f682477fd99
kaanTgun <33357878+kaanTgun@users.noreply.github.com>
2021-03-02 12:34:02 -0500

    none

--- src/Convert2Onnx.py
+++ src/Convert2Onnx.py
@@ -1,13 +1,12 @@
 import torch
-
 from Networks import DQN
 
 def main():
 	model = DQN(lr=0.001, input_dims=4, fc1_dim=256, fc2_dim=256, n_actions=3, checkpoint_path=".", name="DQN")
-	model.load_state_dict(torch.load("./Trained_models/Strategy1/episode_100_200/DQN/DQN_10000_Weights_score_74.848.pt"))
+	model.load_state_dict(torch.load("./Trained_models/Strategy3/episode_100_200/ema6hr_e001/DQN/DQN_10000_Weights_score_30.014.pt", map_location='cpu'))
 	model.eval()
 	dummy_input = torch.zeros(1,4)
-	torch.onnx.export(model,dummy_input, "DQN_Long_S1.onnx", verbose=True )
+	torch.onnx.export(model,dummy_input, "DQN_Long_S3_6hr_e001.onnx", verbose=True)
 if __name__ == '__main__':
 		main();
 		
